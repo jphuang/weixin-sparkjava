6337b15b1f9ba181f0b63c2a1cb00741638e4a11
江平仔 <hjp_008@163.com>
2015-05-07 14:21:03 +0800

    增加趣图实体类

---
+++ src/main/java/entity/OddPhotos.java
@@ -0,0 +1,53 @@
+package entity;
+
+import java.util.Date;
+
+public class OddPhotos {
+	private String title;
+	private String thumburl;
+	private String url;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
+	public String getThumburl() {
+		return thumburl;
+	}
+	public void setThumburl(String thumburl) {
+		this.thumburl = thumburl;
+	}
+	public String toString(String toUser,String fromUser) {
+			String result = "<xml>" + 
+				"<ToUserName><![CDATA[%s]]></ToUserName>" + 
+				"<FromUserName><![CDATA[%s]]></FromUserName>" + 
+				"<CreateTime>%d</CreateTime>" + 
+				"<MsgType><![CDATA[news]]></MsgType>" + 
+				"<ArticleCount>1</ArticleCount>" + 
+				"<Articles>" + 
+				"<item>" + 
+				"<Title><![CDATA[%S]]></Title> " + 
+				"<Description><![CDATA[%s]]></Description>" + 
+				"<PicUrl><![CDATA[%s]]></PicUrl>" + 
+				"<Url><![CDATA[%s]]></Url>" + 
+				"</item>" + 
+				"</Articles>" + 
+				"</xml>";
+			return String.format(result, toUser,fromUser,new Date().getTime(),title,"",thumburl,url);
+		
+	}
+	
+	
+	
+	
+}
