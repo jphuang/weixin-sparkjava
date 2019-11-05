9798ca458d7523f090be278d9036394bc06282c8
huangjiangping <huangjiangping@huya.com>
2019-11-05 20:36:46 +0800

    优化

--- src/main/java/com/jiang/ping/weixin/process/MusicApiProcess.java
+++ src/main/java/com/jiang/ping/weixin/process/MusicApiProcess.java
@@ -50,8 +50,7 @@ public class MusicApiProcess {
         }
 		return null;
 	}
-	private JSONObject  getSongInfo(String songId) throws UnsupportedEncodingException {
-		songId = URLEncoder.encode(songId,"utf-8");
+	private JSONObject  getSongInfo(String songId)  {
 		String url = "https://u.y.qq.com/cgi-bin/musicu.fcg?format=json&data=%7B%22req_0%22%3A%7B%22module%22%3A%22vkey.GetVkeyServer%22%2C%22method%22%3A%22CgiGetVkey%22%2C%22param%22%3A%7B%22guid%22%3A%22358840384%22%2C%22songmid%22%3A%5B%22"+songId+"%22%5D%2C%22songtype%22%3A%5B0%5D%2C%22uin%22%3A%221443481947%22%2C%22loginflag%22%3A1%2C%22platform%22%3A%2220%22%7D%7D%2C%22comm%22%3A%7B%22uin%22%3A%2218585073516%22%2C%22format%22%3A%22json%22%2C%22ct%22%3A24%2C%22cv%22%3A0%7D%7D";
 		logger.info("getSongInfo url:{}" , url);
 		HttpGet request = new HttpGet(url);
